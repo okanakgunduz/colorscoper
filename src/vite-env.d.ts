630f812874a3cd18c4595123f4b3ed68f0626fdf
XenoverseUp <xenoverseup@gmail.com>
2025-01-26 22:17:01 +0300

    update: eslint env ignore

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,28 @@
 /// <reference types="vite/client" />
+
+type JsonifiedValue = T extends number | string | null | boolean
+  ? T
+  : T extends { toJSON(): infer R }
+    ? R
+    : T extends undefined | ((...args: any[]) => any)
+      ? never
+      : T extends object
+        ? JsonifiedObject<T>
+        : never
+
+type JsonifiedObject<T> = {
+  [Key in keyof T as [JsonifiedValue<T[Key]>] extends [never]
+    ? never
+    : Key]: JsonifiedValue<T[Key]>
+}
+
+type Stringified<ObjType> = string & { source: ObjType }
+
+interface JSON {
+  stringify<T>(
+    value: T,
+    replacer?: null | undefined,
+    space?: string | number,
+  ): Stringified<T>
+  parse<T>(str: Stringified<T>, replacer?: null | undefined): JsonifiedObject<T>
+}

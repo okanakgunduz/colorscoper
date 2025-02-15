6391621b8a947dde89804ad18e944d1bf85e6b6c
XenoverseUp <xenoverseup@gmail.com>
2025-02-16 00:37:23 +0300

    feat: padding huewb map

--- src/components/picker/hue-wb-map.tsx
+++ src/components/picker/hue-wb-map.tsx
@@ -1,29 +1,40 @@
 import chroma from "chroma-js"
-import { useMemo } from "react"
+import { motion } from "motion/react"
+import { useMemo, useState } from "react"
 import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch"
 import For from "@components/common/for"
 import useDimensions from "@hooks/useDimensions"
+import { fade } from "@utils/animation"
+import cx from "@utils/cx"
 import linearTo2D from "@utils/linear-to-2d"
 import map from "@utils/map"
 import { useSelectionStore } from "@stores/selection.store"
 
-const CELL_SIZE = 40
+const CELL_SIZE_LARGE = 60
+const CELL_SIZE_SMALL = 30
+
+const getCells = (width: number, height: number, size: number) => ({
+  x: Math.floor(width / size),
+  y: Math.floor(height / size),
+  count: Math.floor(width / size) * Math.floor(height / size),
+})
 
 export default function HueWBMap() {
   const [ref, rect] = useDimensions<HTMLDivElement>()
-  const cells = useMemo(() => {
-    return {
-      x: Math.floor(rect.width / CELL_SIZE) & ~1,
-      y: Math.floor(rect.height / CELL_SIZE),
-      count:
-        (Math.floor(rect.width / CELL_SIZE) & ~1) *
-        Math.floor(rect.height / CELL_SIZE),
-    }
-  }, [rect.width, rect.height])
-
-  const luminosityScale = chroma.scale(["#e5e5e5", "#4a4a4a"]).mode("lab")
-
+  const [isSmall, setIsSmall] = useState(false)
   const setPickerSelection = useSelectionStore.use.setPickerSelection()
+
+  const largeCells = useMemo(
+    () => getCells(rect.width, rect.height, CELL_SIZE_LARGE),
+    [rect.width, rect.height],
+  )
+
+  const smallCells = useMemo(
+    () => getCells(rect.width, rect.height, CELL_SIZE_SMALL),
+    [rect.width, rect.height],
+  )
+
+  const luminosityScale = chroma.scale(["#e0e0e0", "#4a4a4a"]).mode("hsi")
 
   return (
     <div
@@ -32,6 +43,7 @@ export default function HueWBMap() {
     >
       <TransformWrapper
         centerZoomedOut
+        onZoom={(e) => setIsSmall(e.state.scale > 1.5 ? true : false)}
         pinch={{ disabled: true }}
         doubleClick={{
           disabled: true,
@@ -47,43 +59,116 @@ export default function HueWBMap() {
             height: "100%",
           }}
         >
-          <div
-            className="grid size-full gap-[1px] overflow-hidden p-[1px]"
-            style={{
-              gridTemplateColumns: `repeat(${cells.x}, 1fr)`,
-              gridTemplateRows: `repeat(${cells.y}, 1fr)`,
-            }}
-          >
-            <For times={cells.count}>
-              {(i) => {
-                const { x, y } = linearTo2D(i, cells.x)
+          <div className="relative size-full">
+            {/* Large Grid */}
 
-                const hue =
-                  x < Math.floor(cells.x / 2)
-                    ? map(y, 0, cells.y - 1, 0, 150)
-                    : map(y, 0, cells.y - 1, 180, 330)
-
-                const normalizedX =
-                  x < Math.floor(cells.x / 2)
-                    ? x / (cells.x / 2 - 1)
-                    : 1 - (x - cells.x / 2) / (cells.x / 2 - 1)
-
-                const luminosity = luminosityScale(normalizedX).luminance()
-
-                return (
-                  <div
-                    onClick={() =>
-                      setPickerSelection(chroma.hsl(hue, 1, luminosity))
-                    }
-                    key={`hue-wb-map-cell-${i}`}
-                    className="group flex cursor-pointer items-center justify-center rounded text-center text-[8px] transition hover:scale-90"
-                    style={{ background: chroma.hsl(hue, 1, luminosity).css() }}
-                  >
-                    <span className="size-1 rounded-full bg-white/20 transition group-hover:scale-110 group-hover:bg-white group-active:scale-150"></span>
-                  </div>
-                )
+            <motion.div
+              variants={fade}
+              initial="hidden"
+              animate={isSmall ? "hidden" : "visible"}
+              transition={{
+                delay: isSmall ? 0.025 : 0,
+                duration: 0.1,
               }}
-            </For>
+              className={cx(
+                "p-sidebar absolute inset-0 grid gap-[1px] overflow-hidden",
+                {
+                  "pointer-events-none": isSmall,
+                },
+              )}
+              style={{
+                gridTemplateColumns: `repeat(${largeCells.x}, 1fr)`,
+                gridTemplateRows: `repeat(${largeCells.y}, 1fr)`,
+              }}
+            >
+              <For times={largeCells.count}>
+                {(i) => {
+                  const { x, y } = linearTo2D(i, largeCells.x)
+
+                  const hue =
+                    x < Math.floor(largeCells.x / 2)
+                      ? map(y, 0, largeCells.y - 1, 0, 150)
+                      : map(y, 0, largeCells.y - 1, 180, 330)
+
+                  const normalizedX =
+                    x < Math.floor(largeCells.x / 2)
+                      ? x / (largeCells.x / 2 - 1)
+                      : 1 - (x - largeCells.x / 2) / (largeCells.x / 2 - 1)
+
+                  const luminosity = luminosityScale(normalizedX).luminance()
+
+                  return (
+                    <motion.div
+                      onClick={() =>
+                        setPickerSelection(chroma.hsl(hue, 1, luminosity))
+                      }
+                      key={`hue-wb-map-cell-${i}-${CELL_SIZE_LARGE}`}
+                      className="group flex cursor-pointer items-center justify-center rounded text-center text-[8px] transition hover:scale-90"
+                      style={{
+                        background: chroma.hsl(hue, 1, luminosity).css(),
+                      }}
+                    >
+                      <span className="size-1 rounded-full bg-white/20 transition group-hover:scale-110 group-hover:bg-white group-active:scale-150"></span>
+                    </motion.div>
+                  )
+                }}
+              </For>
+            </motion.div>
+
+            {/* Small Grid */}
+
+            <motion.div
+              variants={fade}
+              initial="hidden"
+              animate={!isSmall ? "hidden" : "visible"}
+              transition={{
+                delay: !isSmall ? 0.025 : 0,
+                duration: 0.1,
+              }}
+              className={cx(
+                "p-sidebar absolute inset-0 grid gap-[0.5px] overflow-hidden",
+                {
+                  "pointer-events-none": !isSmall,
+                },
+              )}
+              style={{
+                gridTemplateColumns: `repeat(${smallCells.x}, 1fr)`,
+                gridTemplateRows: `repeat(${smallCells.y}, 1fr)`,
+              }}
+            >
+              <For times={smallCells.count}>
+                {(i) => {
+                  const { x, y } = linearTo2D(i, smallCells.x)
+
+                  const hue =
+                    x < Math.floor(smallCells.x / 2)
+                      ? map(y, 0, smallCells.y - 1, 0, 150)
+                      : map(y, 0, smallCells.y - 1, 180, 330)
+
+                  const normalizedX =
+                    x < Math.floor(smallCells.x / 2)
+                      ? x / (smallCells.x / 2 - 1)
+                      : 1 - (x - smallCells.x / 2) / (smallCells.x / 2 - 1)
+
+                  const luminosity = luminosityScale(normalizedX).luminance()
+
+                  return (
+                    <motion.div
+                      onClick={() =>
+                        setPickerSelection(chroma.hsl(hue, 1, luminosity))
+                      }
+                      key={`hue-wb-map-cell-${i}-${CELL_SIZE_LARGE}`}
+                      className="group flex cursor-pointer items-center justify-center rounded-xs text-center text-[8px] transition hover:scale-90"
+                      style={{
+                        background: chroma.hsl(hue, 1, luminosity).css(),
+                      }}
+                    >
+                      <span className="size-0.5 rounded-full bg-white/20 transition group-hover:scale-110 group-hover:bg-white group-active:scale-150"></span>
+                    </motion.div>
+                  )
+                }}
+              </For>
+            </motion.div>
           </div>
         </TransformComponent>
       </TransformWrapper>

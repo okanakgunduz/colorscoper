f70f8ce2ea35e2d13888095de1f1f277bc3f3345
XenoverseUp <xenoverseup@gmail.com>
2025-02-15 23:45:49 +0300

    feat: huewb map picking

--- src/components/picker/hue-wb-map.tsx
+++ src/components/picker/hue-wb-map.tsx
@@ -5,8 +5,9 @@ import For from "@components/common/for"
 import useDimensions from "@hooks/useDimensions"
 import linearTo2D from "@utils/linear-to-2d"
 import map from "@utils/map"
+import { useSelectionStore } from "@stores/selection.store"
 
-const CELL_SIZE = 50
+const CELL_SIZE = 40
 
 export default function HueWBMap() {
   const [ref, rect] = useDimensions<HTMLDivElement>()
@@ -20,12 +21,22 @@ export default function HueWBMap() {
     }
   }, [rect.width, rect.height])
 
+  const luminosityScale = chroma.scale(["#e5e5e5", "#4a4a4a"]).mode("lab")
+
+  const setPickerSelection = useSelectionStore.use.setPickerSelection()
+
   return (
     <div
       className="size-full touch-none overflow-hidden overscroll-none"
       ref={ref}
     >
-      <TransformWrapper centerZoomedOut pinch={{ disabled: true }}>
+      <TransformWrapper
+        centerZoomedOut
+        pinch={{ disabled: true }}
+        doubleClick={{
+          disabled: true,
+        }}
+      >
         <TransformComponent
           wrapperStyle={{
             width: "100%",
@@ -37,7 +48,7 @@ export default function HueWBMap() {
           }}
         >
           <div
-            className="grid size-full overflow-hidden p-1"
+            className="grid size-full gap-[1px] overflow-hidden p-[1px]"
             style={{
               gridTemplateColumns: `repeat(${cells.x}, 1fr)`,
               gridTemplateRows: `repeat(${cells.y}, 1fr)`,
@@ -46,24 +57,30 @@ export default function HueWBMap() {
             <For times={cells.count}>
               {(i) => {
                 const { x, y } = linearTo2D(i, cells.x)
+
                 const hue =
                   x < Math.floor(cells.x / 2)
                     ? map(y, 0, cells.y - 1, 0, 150)
                     : map(y, 0, cells.y - 1, 180, 330)
 
-                const luminosity =
+                const normalizedX =
                   x < Math.floor(cells.x / 2)
-                    ? map(x, 0, cells.x / 2 - 1, 0.8, 0.05)
-                    : map(x, cells.x / 2, cells.x - 1, 0.05, 0.8)
+                    ? x / (cells.x / 2 - 1)
+                    : 1 - (x - cells.x / 2) / (cells.x / 2 - 1)
+
+                const luminosity = luminosityScale(normalizedX).luminance()
 
                 return (
                   <div
+                    onClick={() =>
+                      setPickerSelection(chroma.hsl(hue, 1, luminosity))
+                    }
                     key={`hue-wb-map-cell-${i}`}
-                    className="flex cursor-pointer items-center justify-center rounded text-center text-[8px] hover:opacity-80"
-                    style={{
-                      background: chroma.hsl(hue, 1, luminosity).css(),
-                    }}
-                  ></div>
+                    className="group flex cursor-pointer items-center justify-center rounded text-center text-[8px] transition hover:scale-90"
+                    style={{ background: chroma.hsl(hue, 1, luminosity).css() }}
+                  >
+                    <span className="size-1 rounded-full bg-white/20 transition group-hover:scale-110 group-hover:bg-white group-active:scale-150"></span>
+                  </div>
                 )
               }}
             </For>
